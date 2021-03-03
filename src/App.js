ce741443e590add6072b228d34ec65d743249d4c
Kristof Kruller <kristof.kruller@gmail.com>
2021-03-03 20:34:40 +0100

    unique logo and hamb import and cs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
 
 const App = () => {
     return (
-        <Navbar>
+        <>
+            <GlobalStyle />
+            <Navbar>
 
-        </Navbar>
+            </Navbar>
+        </>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,81 @@ import React from 'react';
 import styled, {css} from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 import { menuData } from '../data/MenuData';
-import { Button } from './Button'
+import { Button } from './Button';
+import Bars from '../images/hambMenu.svg';
+import LogoSvg from '../images/retroLogoNav.svg'
 
 /*S Styled Components*/
 
+const Nav = styled.nav`
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
+    background: black;
+`;
+
+const NavBtn = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 24px;
+    
+    @media screen and (max-width: 768px) {
+        display: none;
+    } 
+`;
+
+const Logo = styled.i`
+    display: block;
+    background-image: url(${LogoSvg});
+    background-size: contain;
+    height: 60px;
+    width: 60px;
+    cursor: pointer;
+    position: absolute;
+    transform: translate(-30%, 15%);
+    z-index: 100;
+    position: fixed;
+    top: 0%;
+    left: 5%;
+    background-repeat: no-repeat;
+
+    @media screen and (max-width: 768px) {
+        left: 15%;
+    } 
+`;
+
+const MenuBars = styled.i`
+    display: none;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+        background-image: url(${Bars});
+        background-size: contain;
+        height: 40px;
+        width: 40px;
+        cursor: pointer;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-50%, 25%);
+        background-repeat: no-repeat;
+    } 
+
+`;
+
+const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    } 
+`;
 const NavLink = css`
     color: #fff;
     display: flex;
@@ -16,38 +87,8 @@ const NavLink = css`
     text-decoration: none;
 `;
 
-const Logo = styled(Link)`
-    ${NavLink}
-    font-style: italic;
-`;
-
-const MenuBars = styled.i``;
-
-const NavMenu = styled.div`
-    display: flex;
-    align-items: center;
-
-`;
-
 const NavMenuLinks = styled(Link)`
     ${NavLink}
-`;
-
-const Nav = styled.nav`
-    height: 60px;
-    display: flex;
-    justify-content: space-between;
-    padding: 1rem 2rem;
-    z-index: 100;
-    position: fixed;
-    width: 100%;
-    background: #000;
-`;
-
-const NavBtn = styled.div`
-    display: flex;
-    align-items: center;
-    margin-right: 24px;
 `;
 
 /*E Styled Components*/
@@ -55,7 +96,7 @@ const NavBtn = styled.div`
 const Navbar = () => {
     return (
         <Nav>
-           <Logo to="/">logoPLCHLDR</Logo>
+           <Logo to="/"></Logo>
            <MenuBars></MenuBars>
            <NavMenu>
             {menuData.map((item, index) => (
@@ -65,7 +106,7 @@ const Navbar = () => {
             ))}
            </NavMenu>
            <NavBtn>
-               <Button to="/contact">Contact Us</Button>
+               <Button to="/contact">Get in touch</Button>
            </NavBtn>
         </Nav>
     )
